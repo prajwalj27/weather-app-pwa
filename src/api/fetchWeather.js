ecc6b7d333a0996ba4d0cab23961f6a8367168fb
Prajwal Jaiswal <prajwalmjaiswal@gmail.com>
2022-02-22 15:24:22 +0530

    Add all files

---
+++ src/api/fetchWeather.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const URL = process.env.REACT_APP_URL;
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+export const fetchWeather = async (query) => {
+  const { data } = await axios.get(URL, {
+    params: {
+      q: query,
+      units: 'metric',
+      APPID: API_KEY,
+    },
+  });
+
+  return data;
+};
